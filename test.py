c39d9eddbb6da6aab642da9e9abad7b761c881aa
trungthanhho <hothanhtrung09@gmail.com>
2025-04-09 15:48:04 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_excel(r"C:\Users\DN-CDS-XNBB1\Downloads\DANH_MUC_SAN_PHAM_FINAL_DATA.xlsx")
+
+# Lấy 4 cột đầu tiên
+first_four_columns = df.iloc[:, :4]
+
+print(first_four_columns.head())  # In 5 dòng đầu của 4 cột đầu
