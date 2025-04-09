68c6a37669b85ee74e266a7ecb5ffe79bf423503
trungthanhho <hothanhtrung09@gmail.com>
2025-04-09 16:03:25 +0700

    update test comment

--- test.py
+++ test.py
@@ -6,3 +6,5 @@ df = pd.read_excel(r"C:\Users\DN-CDS-XNBB1\Downloads\DANH_MUC_SAN_PHAM_FINAL_DAT
 first_four_columns = df.iloc[:, :4]
 
 print(first_four_columns.head())  # In 5 dòng đầu của 4 cột đầu
+
+# test github
